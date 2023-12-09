9d527b3c7a5f55d4f4904794996eea672402060d
Abdallah Hussein <abiza2013@gmail.com>
2023-12-10 00:28:57 +0200

    convertToGrayscale

--- src/AveragePixelsWithoutSeparation.java
+++ src/AveragePixelsWithoutSeparation.java
@@ -3,34 +3,67 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 import javax.imageio.ImageIO;
 
 public class AveragePixelsWithoutSeparation {
-    public static void main(String[] args) {
-        try {
-            File file = new File("C:\\Users\\ahmal\\IdeaProjects\\VectorQuantization\\src\\input.jpg");
-            // Load the image
-            BufferedImage image = VectorQuantization.loadImage(Paths.get("C:\\Users\\ahmal\\IdeaProjects\\VectorQuantization\\src\\input.jpg"));
 
-            // Get ARGB values of two pixels (for example, pixels at coordinates (10,10) and (20,20))
-            int pixel1 = image.getRGB(10, 10);
-            int pixel2 = image.getRGB(20, 20);
 
-            // Calculate average ARGB values
-            int avgPixel = ((pixel1 & 0xFFFEFEFF) + (pixel2 & 0xFFFEFEFF)) / 2; // Average without alpha
+  public static void main(String[] args) {
+    try {
+      Scanner scan = new Scanner(System.in);
+      int codeBookSize = scan.nextInt();
+//    Vector Dimensions (n*m) (block)
+      int n = scan.nextInt();
+      int m = scan.nextInt();
+      BufferedImage inputImage = VectorQuantization.loadImage(Paths.get("src/input1.jpg"));
+      int width = inputImage.getWidth();
+      int height = inputImage.getHeight();
 
-            // Create a new pixel with the average ARGB value
-            BufferedImage outputImage = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
-            outputImage.setRGB(0, 0, avgPixel);
+      BufferedImage sub = inputImage.getSubimage(0 , 0, width, height);
+      File fileout = new File("src/output.jpg");
 
-            // Save the new pixel to an output image
-            VectorQuantization.saveImage(Paths.get("C:\\Users\\ahmal\\IdeaProjects\\VectorQuantization\\src\\output.jpg") ,
-                    outputImage, "jpg");
+//      BufferedImage image2 = new BufferedImage(w, h, BufferedImage.TYPE_3BYTE_BGR);
+//
+//      for (int x = 0; x < h; x++)
+//        for (int y = 0; y < w; y++) {
+//          int value = 0xff000000 | ((int) pixels[x][y] << 16) | ((int) pixels[x][y] << 8) | (int) pixels[x][y];
+//          image2.setRGB(y, x, value);
+//        }
+//      System.out.println("mmmm");
+  convertToGrayscale(sub);
+      ImageIO.write(sub, "jpg", fileout);
 
-            System.out.println("Average pixel image saved successfully.");
-        } catch (IOException e) {
-            System.out.println("Error occurred: " + e.getMessage());
-        }
+      // Create the output image with the same dimensions as the input image
+//            BufferedImage outputImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+//
+//            // Set the pixel values of the output image to match the input image
+//            outputImage.setRGB(0, 0, width, height, blocks, 0, width);
+//
+//            // Save the new image to an output file
+//            VectorQuantization.saveImage(Paths.get("src/output.jpg"), outputImage, "jpg");
+//
+//            System.out.println("Output image saved successfully.");
+    } catch (IOException e) {
+      System.out.println("Error occurred: " + e.getMessage());
     }
+
+  }
+  public static float[][] convertToGrayscale(BufferedImage image) {
+    int width = image.getWidth();
+    int height = image.getHeight();
+    float[][] grayscalePixels = new float[height][width];
+
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        int rgb = image.getRGB(j, i);
+        int pixel = rgb & 0xFF;
+        grayscalePixels[i][j] = pixel;
+      }
+    }
+    return grayscalePixels;
+  }
 }
+
+
 
